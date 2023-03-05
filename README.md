e5855f4dba96f548f6888dd483a5b4e8ac901be5
khouloudAmamouHaddad <khouloud.amamouhaddad@gmail.com>
2023-03-05 15:24:22 +0100

    Class Methods

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 A sample command-line application with an entrypoint in `bin/`, library code
 in `lib/`, and example unit test in `test/`.
+
+<h5>Commands</h5>
+<ol>
+<li>dart create <options> <name_of_project></li>
+<li>dart run</li>
+<li>
+<p><strong>OOP Basics</strong></p>
+- 1- var
+- 2- method
+- 3- Set and Get
+- 4- Constructor
+
+</li>
+</ol>
--- bin/app.dart
+++ bin/app.dart
@@ -1,5 +1,21 @@
 import 'package:app/app.dart' as app;
 
-void main(List<String> arguments) {
-  print('Hello world: ${app.calculate()}!');
+void main(/*List<String> arguments*/) {
+  Mobile mobile = new Mobile();
+
+  mobile.printScreen();
+
+  print("and Name: $mobile.name");
+
+  //print('Hello world: ${app.calculate()}!');
+}
+
+//OOP
+class Mobile {
+  double screen = 6.4;
+  String name = "Iphone pro Max";
+
+  void printScreen() {
+    screen > 5.6 ? print("Large Screen") : print("Small Screen");
+  }
 }
