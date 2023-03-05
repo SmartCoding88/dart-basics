1150dc09d91c96a98dae5bbf9ef2cf636d6ce507
khouloudAmamouHaddad <khouloud.amamouhaddad@gmail.com>
2023-03-05 16:05:21 +0100

    Multi level Inheritance

--- bin/app.dart
+++ bin/app.dart
@@ -1,34 +1,21 @@
 import 'package:app/app.dart' as app;
 
 void main(/*List<String> arguments*/) {
-  Samsung s20 = new Samsung();
-  s20.camera = "S20Camera";
-
-  print(s20.camera);
-
-  s20.memory = "8G";
-
-  s20.printMobileMemory();
+  Three three = new Three();
+  three.varone = "one Test";
+  print(three.varone);
 }
 
 //OOP
-class Mobile {
-  String screen = "6.3";
-  String camera = "";
-  String cpu = "";
-  String memory = "8G";
 
-  void printMobileMemory() {
-    print(this.memory);
-  }
+class One {
+  String varone = "";
 }
 
-class Samsung extends Mobile {
-  String brand = "Samsung";
-  String memory = "16G";
-  //override
-  @override
-  void printMobileMemory() {
-    print("Memory: $memory");
-  }
+class Two extends One {
+  String vartwo = "";
+}
+
+class Three extends Two {
+  String arthree = "";
 }
